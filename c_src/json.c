3cf95caa3004fbc038432074ac432002799a5786
Paul J. Davis <paul.joseph.davis@gmail.com>
2011-03-11 11:16:02 -0500

    Fix error for upgrades and reloads.
    If the reload and upgrade functions are NULL they will cause any
    attempt at reloading or upgrading the NIF to fail. We don't have
    any sort of state that needs to be managed so simple definitions
    that simply return 0 should be all that's required.

--- c_src/json.c
+++ c_src/json.c
@@ -4,7 +4,19 @@ ERL_NIF_TERM encode(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]);
 ERL_NIF_TERM decode(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]);
 
 int
-on_load(ErlNifEnv* env, void** priv_data, ERL_NIF_TERM load_info)
+on_load(ErlNifEnv* env, void** priv_data, ERL_NIF_TERM info)
+{
+    return 0;
+}
+
+int
+on_reload(ErlNifEnv* env, void** priv_data, ERL_NIF_TERM info)
+{
+    return 0;
+}
+
+int
+on_upgrade(ErlNifEnv* env, void** priv_data, void** old_data, ERL_NIF_TERM info)
 {
     return 0;
 }
@@ -15,4 +27,4 @@ static ErlNifFunc nif_funcs[] =
     {"decode", 1, decode}
 };
 
-ERL_NIF_INIT(json, nif_funcs, &on_load, NULL, NULL, NULL);
+ERL_NIF_INIT(json, nif_funcs, &on_load, &on_reload, &on_upgrade, NULL);
