12fc0049bbf1c822a3dac67c627cbe3e85766ba3
YAMASHINA Hio <hio@hio.jp>
2012-04-05 09:56:19 +0900

    fix test.

--- test/json_tests.erl
+++ test/json_tests.erl
@@ -1,6 +1,8 @@
 -module(json_tests).
 -include_lib("eunit/include/eunit.hrl").
 
-json_test() ->
-    ?assert(true),
-    ok.
+json_test_() ->
+  [
+    ?_assertEqual({module, json}, code:ensure_loaded(json)),
+    ?_assert(true)
+  ].
